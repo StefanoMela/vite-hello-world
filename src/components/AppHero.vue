6a6c66e870e69b952815bed573b28c21a34cc872
StefanoMela <stefano.mela25@gmail.com>
2023-09-08 00:30:32 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+
+      image: '../assets/img/haotian-zheng-Dr7TUl4BURo-unsplash.jpg'
+    };
+  },
+
+  methods: {
+
+    buildImagePath (path) {
+
+      const imageUrl = new URL('../assets/img/haotian-zheng-Dr7TUl4BURo-unsplash.jpg', import.meta.url);
+
+      return imageUrl.href
+
+    },
+  }
+};
+</script>
+
+<template>
+  <div class="container d-flex justify-content-center">
+    <img :src="buildImagePath(image)" alt="">
+  </div>
+</template>
+
+<style scoped>
+
+img {
+
+  max-width: 750px;
+
+};
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  }
+}
+
+</script>
+
+<template>
+    <h1 class="text-center my-2">Hello Wor..Vue</h1>
+</template>
+
+<style></style>
